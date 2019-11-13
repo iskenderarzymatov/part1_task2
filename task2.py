c9b09a0dd602be52ad271789f6508e28d827e0f9
iskenderarzymatov <iskenderarzymatov@gmail.com>
2019-11-13 09:50:20 -0500

    part1_task2

---
+++ task2.py
@@ -0,0 +1,9 @@
+
+def square_number(isken): return isken ** 2
+
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+print("YOUR CODE IS CORRECT!")
+test_square_number()
